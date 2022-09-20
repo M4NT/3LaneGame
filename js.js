cebcb93559def81ede20a4d85bb46242077d56a7
Your Name <you@example.com>
2022-09-20 19:24:27 -0300

    js

--- js.js
+++ js.js
@@ -1,26 +1,28 @@
-function checar(){
-    var respostaUsuario = document.getElementById("resposta").value;
-    var respostaCerta1 = "Cubone"
-    var respostaCerta2 = "Mewtwo"
-    var respostaCerta3 = "Treecko"
+function checar() {
+	var respostaUsuario = document.getElementById('resposta').value;
+	var respostaCerta1 = 'cubone';
+	var respostaCerta2 = 'mewtwo';
+	var respostaCerta3 = 'treecko';
 
-    if ( respostaUsuario.toLowerCase() === respostaCerta1 || respostaCerta2 || respostaCerta3) {
-        alert("Parabéns, você acertou!")
-    } else {
-        alert("Parabéns, você errou!")
-    }
+	if (
+		respostaUsuario.toLowerCase() === respostaCerta1 ||
+		respostaCerta2 ||
+		respostaCerta3
+	) {
+		alert('Parabéns, você acertou!');
+	} else {
+		alert('Parabéns, você errou!');
+	}
 }
 
 function mudarConteudo1() {
-    document.getElementById("dica1").innerHTML = "Tem rabo";
-    document.getElementById("dica2").innerHTML = "É Único";
-    document.getElementById("dica3").innerHTML = "Ash deu um soco";
+	document.getElementById('dica1').innerHTML = 'Tem rabo';
+	document.getElementById('dica2').innerHTML = 'É Único';
+	document.getElementById('dica3').innerHTML = 'Ash deu um soco';
 }
 
 function mudarConteudo2() {
-    document.getElementById("dica1").innerHTML = "Tipo Planta";
-    document.getElementById("dica2").innerHTML = "Tem rabo";
-    document.getElementById("dica3").innerHTML = "É inicial";
+	document.getElementById('dica1').innerHTML = 'Tipo Planta';
+	document.getElementById('dica2').innerHTML = 'Tem rabo';
+	document.getElementById('dica3').innerHTML = 'É inicial';
 }
-
-
